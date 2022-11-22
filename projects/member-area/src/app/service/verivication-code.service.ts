dd6aedb208d9f5930cfd7236cff6c15027fdbcab
triajidhan <triajidhan@gmail.com>
2022-11-22 09:33:17 +0700

    set-register

---
+++ projects/member-area/src/app/service/verivication-code.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from "@angular/common/http"
+import { Injectable } from "@angular/core"
+import { BASE_URL } from "projects/constant/base-url"
+import { Observable } from "rxjs"
+
+@Injectable({
+    providedIn: 'root'
+})
+export class VerificationCodeService {
+
+    constructor(private http: HttpClient) { }
+
+    generateCode(data: any): Observable<any> {
+        return this.http.get<any>(`${BASE_URL.LOCALHOST}/verification-code/generate/${data}`)
+    }
+
+    validate(data: any): Observable<any> {
+        return this.http.get<any>(`${BASE_URL.LOCALHOST}/verification-code/validate/${data}`)
+    }
+}
