cf54a1bfaf728967faaeb164d0a488edca65a861
irwansyah_10010 <irwanbae33@gmail.com>
2022-11-23 10:08:39 +0700

    create and update service

--- projects/main-area/src/app/service/article.service.ts
+++ projects/main-area/src/app/service/article.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http"
 import { Injectable } from "@angular/core"
-import { BASE_URL } from "projects/constant/base-url"
-import { Article } from "projects/interface/article"
+import { BASE_URL } from "../../../../../projects/constant/base-url"
+import { Article } from "../../../../../projects/interface/article"
 import { ResponseMessage } from "projects/interface/response-messge"
 import { Observable } from "rxjs"
 
@@ -13,7 +13,23 @@ export class ArticleService {
   constructor(private http: HttpClient) { }
 
   getAll(startPosition: number, limit: number): Observable<Article> {
-    return this.http.get<Article>(`${Object.values(BASE_URL)[0]}/articles?startPostion=${startPosition}&limit=${limit}`)
+    return this.http.get<Article>(`${Object.values(BASE_URL)[0]}/articles?startPosition=${startPosition}&limit=${limit}`)
+  }
+
+  getById(id: number): Observable<Article> {
+    return this.http.get<Article>(`${Object.values(BASE_URL)[0]}/${id}/articles/get`)
+  }
+
+  getByArticleCode(articleCode:string): Observable<Article> {
+    return this.http.get<Article>(`${Object.values(BASE_URL)[0]}/articles/article-code/$articleCode=${articleCode}`)
+  }
+
+  getTotal(): Observable<Article> {
+    return this.http.get<Article>(`${Object.values(BASE_URL)[0]}/articles/total-article`)
+  }
+
+  getByIsActive(startPosition: number, limit: number): Observable<Article> {
+    return this.http.get<Article>(`${Object.values(BASE_URL)[0]}/articles/is-active/?startPosition=${startPosition}&limit=${limit}`)
   }
 
   insert(data: any): Observable<ResponseMessage> {
@@ -24,15 +40,5 @@ export class ArticleService {
     return this.http.put<ResponseMessage>(`${Object.values(BASE_URL)[0]}/articles`, data)
   }
 
-  getById(id: number): Observable<Article> {
-    return this.http.get<Article>(`${Object.values(BASE_URL)[0]}/${id}/articles/get`)
-  }
 
-  getTotal(): Observable<Article> {
-    return this.http.get<Article>(`${Object.values(BASE_URL)[0]}/articles/total-article`)
-  }
-
-  getByArticleCode(articleCode:string): Observable<Article> {
-    return this.http.get<Article>(`${Object.values(BASE_URL)[0]}/articles/article-code/$articleCode=${articleCode}`)
-  }
 }
---
+++ projects/main-area/src/app/service/like.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BASE_URL } from "../../../../../projects/constant/base-url";
+
+import { ResponseMessage } from "projects/interface/response-messge";
+import { Observable } from "rxjs";
+import { Like } from "projects/interface/like";
+
+
+@Injectable({
+    providedIn:"root"
+})
+
+export class LikeService{
+    constructor(private http: HttpClient) { }
+
+    getAll(startPosition: number, limit: number): Observable<Like> {
+      return this.http.get<Like>(`${Object.values(BASE_URL)[0]}/likes?startPosition=${startPosition}&limit=${limit}`)
+    }
+  
+    getById(id: string): Observable<Like> {
+      return this.http.get<Like>(`${Object.values(BASE_URL)[0]}/${id}/likes/get`)
+    }
+
+    getByUser(userId: string): Observable<Like> {
+        return this.http.get<Like>(`${Object.values(BASE_URL)[0]}/likes/users/?userId=${userId}`)
+    }
+
+    getByPost(postId: string): Observable<Like> {
+        return this.http.get<Like>(`${Object.values(BASE_URL)[0]}/likes/users/?postId=${postId}`)
+    }
+  
+    getTotalByUser(userId: string): Observable<Like> {
+      return this.http.get<Like>(`${Object.values(BASE_URL)[0]}/likes/total-users/?userId=${userId}`)
+    }
+
+    getTotalByPost(postId: string): Observable<Like> {
+        return this.http.get<Like>(`${Object.values(BASE_URL)[0]}/likes/total-posts/?postId=${postId}`)
+    }
+
+    getUserLikePost(postId: string,userId:string): Observable<Like> {
+        return this.http.get<Like>(`${Object.values(BASE_URL)[0]}/likes/user-like/?postId=${postId}&userId=${userId}`)
+    }
+  
+    getByIsActive(startPosition: number, limit: number): Observable<Like> {
+      return this.http.get<Like>(`${Object.values(BASE_URL)[0]}/likes/is-active/?startPosition=${startPosition}&limit=${limit}`)
+    }
+  
+    insert(data: any): Observable<ResponseMessage> {
+      return this.http.post<ResponseMessage>(`${Object.values(BASE_URL)[0]}/likes`, data)
+    }
+  
+    update(data: any): Observable<ResponseMessage> {
+      return this.http.put<ResponseMessage>(`${Object.values(BASE_URL)[0]}/likes`, data)
+    }
+}
