adab49291409c7193b87cdfe4b5f34be32d80736
triajidhan <triajidhan@gmail.com>
2022-11-24 15:59:28 +0700

    insert-post

---
+++ projects/main-area/src/app/service/post-type.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http"
+import { Injectable } from "@angular/core"
+import { BASE_URL } from "projects/constant/base-url"
+import { Observable } from "rxjs"
+
+@Injectable({
+    providedIn: "root"
+})
+
+export class PostTypeService {
+
+    constructor(private httpClient: HttpClient) { }
+
+    getAll(): Observable<any> {
+        return this.httpClient.get<any>(`${Object.values(BASE_URL)[0]}/post-types/`)
+    }
+}
--- projects/member-area/src/app/pages/home/home.component.ts
+++ projects/member-area/src/app/pages/home/home.component.ts
@@ -1,30 +1,47 @@
-import { Component, OnInit } from "@angular/core"
-import { FormBuilder, Validators } from "@angular/forms"
+import { Component, OnDestroy, OnInit } from "@angular/core"
+import { FormArray, FormBuilder, Validators } from "@angular/forms"
 import { ActivatedRoute } from "@angular/router"
 import { MenuItem, PrimeNGConfig } from "primeng/api"
+import { PostType } from "projects/interface/post-type"
+import { PostTypeService } from "projects/main-area/src/app/service/post-type.service"
+import { PostService } from "projects/main-area/src/app/service/post.service"
+import { Subscription } from "rxjs"
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
     styleUrls: ['../../../styles.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
     items!: MenuItem[]
     type!: string
+    postType!: string
     display: boolean = false
+    // resultExtension!: string
+    // resultFile !: string
+    fileArray: any[] = [];
+    postTypesRes!: PostType[]
+    postTypes: any[] = []
+
+    private postInsertSubs?: Subscription
+    private postAttachInsertSubs?: Subscription
+    private getAllPostTypeSubs?: Subscription
 
     postForm = this.fb.group({
-        fullName: ['', Validators.required],
-        email: ['', Validators.required],
-        pass: ['', Validators.required],
-        company: ['', Validators.required],
-        industryId: [''],
-        positionId: ['']
+        title: ['', Validators.required],
+        contents: ['', Validators.required],
+        postType: this.fb.group({
+            id: ['']
+        }),
+        titlePoll: [''],
+        postTypeId: [''],
+        file: this.fb.array([])
     })
 
+
     constructor(private primengConfig: PrimeNGConfig, private activatedRoute: ActivatedRoute,
-        private fb: FormBuilder) { }
+        private fb: FormBuilder, private postService: PostService, private postTypeService: PostTypeService) { }
 
     ngOnInit(): void {
         this.primengConfig.ripple = true
@@ -37,13 +54,72 @@ export class HomeComponent implements OnInit {
 
         this.activatedRoute.params.subscribe(result => {
             this.type = result['type']
-            this.init()
+        })
+
+        this.getAllPostTypeSubs = this.postTypeService.getAll().subscribe(result => {
+            this.postTypesRes = result
+            for (let i = 0; i < this.postTypesRes.length; i++) {
+                this.postTypes.push({
+                    postTypeName: this.postTypesRes[i].postTypeName,
+                    postTypeCode: this.postTypesRes[i].postTypeCode,
+                    id: this.postTypesRes[i].id
+                })
+            }
         })
     }
 
-    init() { }
+    fileUpload(event: any): void {
 
-    postDialog() {
+        for (let i = 0; i < event.files.length; i++) {
+            this.fileUploadMultiple(event, i).then(result => {
+                this.fileArray.push({ ext: result[0], files: result[1] })
+            })
+        }
+        console.log(this.fileArray);
+    }
+
+    async fileUploadMultiple(event: any, index: number) {
+        const file: [string, string] = ['', '']
+        const toBase64 = (file: File) => new Promise<string>((resolve, reject) => {
+            const reader = new FileReader()
+            reader.readAsDataURL(file)
+            reader.onload = () => {
+                if (typeof reader.result === "string") resolve(reader.result)
+            }
+            reader.onerror = error => reject(error)
+        })
+        const result = await toBase64(event.files[index])
+        const resultStr = result.substring(result.indexOf(",") + 1, result.length)
+        const resultExt = result.substring(result.indexOf("/") + 1, result.indexOf(";"))
+        file[0] = resultExt
+        file[1] = resultStr
+        return file
+    }
+
+    postDialog(type: string) {
+        this.postType = type
         this.display = true
     }
+
+    postInsert() {
+        this.postForm.patchValue({
+            postType: {
+                id: this.postForm.value.postTypeId
+            },
+        })
+        this.postForm.value.file = this.fileArray;
+
+        console.log(this.postForm.value)
+        this.postInsertSubs = this.postService.insert(this.postForm.value).subscribe(() => {
+            this.display = false
+        })
+
+    }
+
+    ngOnDestroy(): void {
+        this.postInsertSubs?.unsubscribe()
+        this.postAttachInsertSubs?.unsubscribe()
+        this.getAllPostTypeSubs?.unsubscribe()
+    }
+
 }
