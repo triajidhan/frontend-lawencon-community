e5c7299d0a3a6020001dc2eceeccd16320799ec4
irwansyah_10010 <irwanbae33@gmail.com>
2022-11-25 18:42:03 +0700

    home-component

--- projects/member-area/src/app/pages/home/home.component.ts
+++ projects/member-area/src/app/pages/home/home.component.ts
@@ -6,7 +6,9 @@ import { InitEditableRow } from "primeng/table"
 import { BASE_URL } from "projects/constant/base-url"
 import { Post } from "projects/interface/post"
 import { PostType } from "projects/interface/post-type"
+import { ApiService } from "projects/main-area/src/app/service/api.service"
 import { FileService } from "projects/main-area/src/app/service/file.service"
+import { LikeService } from "projects/main-area/src/app/service/like.service"
 import { PostAttachmentService } from "projects/main-area/src/app/service/post-attachment.service"
 import { PostTypeService } from "projects/main-area/src/app/service/post-type.service"
 import { PostService } from "projects/main-area/src/app/service/post.service"
@@ -20,6 +22,8 @@ import { UserService } from "../../service/user.service"
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
+    myId: string = ""
+
     startPosition = 0
     limit = 2
 
@@ -29,7 +33,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     display: boolean = false
     // resultExtension!: string
     // resultFile !: string
-    
+
     urlFile = `${BASE_URL.LOCALHOST}/files/download/`
 
     fileArray: any[] = [];
@@ -37,12 +41,22 @@ export class HomeComponent implements OnInit, OnDestroy {
     postTypes: any[] = []
 
     post: any[] = []
-    user:any[] = []
-    postAttachment:any[] = []
+    user: any[] = []
+    postAttachment: any[] = []
 
-    userObj:Object = new Object()
-    postAttachmentObj:Object = new Object()
+    addLike = this.fb.group({
+        post: this.fb.group({
+            id: ['']
+        })
+    })
 
+    updateLike = this.fb.group({
+        id: [''],
+        isActive: [false]
+    })
+
+    userObj: Object = new Object()
+    postAttachmentObj: Object = new Object()
 
     private postInsertSubs?: Subscription
     private postAttachInsertSubs?: Subscription
@@ -50,8 +64,14 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     private getPostDataSubs?: Subscription
     private getPostAttachmentDataSubs?: Subscription
+    private getCountLikeDataSubs?: Subscription
+    private getUserDataSubs?: Subscription
 
-    private getUserDataSubs?:Subscription
+    private insertLikeDataSubs?: Subscription
+    private updateLikeDataSubs?: Subscription
+    private getIdLikeDataSubs?: Subscription
+
+    private getLikeDataSubs?: Subscription
 
     postForm = this.fb.group({
         title: ['', Validators.required],
@@ -67,16 +87,30 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     constructor(private primengConfig: PrimeNGConfig, private activatedRoute: ActivatedRoute,
         private fb: FormBuilder, private postService: PostService, private postTypeService: PostTypeService,
-        private userService:UserService,private fileService:FileService,private postAttachmentService:PostAttachmentService) { }
+        private userService: UserService, private fileService: FileService, private postAttachmentService: PostAttachmentService,
+        private apiService: ApiService, private likeService: LikeService) { }
 
     ngOnInit(): void {
         this.primengConfig.ripple = true
+
+        this.myId = String(this.apiService.getId())
+
+        console.log(this.myId)
 
         this.items = [
             { label: 'Thread', routerLink: '/homes/threads' },
             { label: 'Likes', routerLink: '/homes/likes' },
             { label: 'Bookmark', routerLink: '/homes/bookmarks' }
         ]
+        this.init();
+    }
+
+    onScroll() {
+        this.startPosition += this.limit
+        this.init()
+    }
+
+    init() {
 
         this.activatedRoute.params.subscribe(result => {
             this.type = result['type']
@@ -94,26 +128,29 @@ export class HomeComponent implements OnInit, OnDestroy {
             }
         })
 
-        this.init();
-    }
-
-    onScroll() {
-        this.startPosition += this.limit
-        this.init()
-    }
-
-    init() {
-        this.getPostDataSubs = this.postService.getIsActiveAndOrder(this.startPosition, this.limit,true).subscribe(result => {
-            for(let i = 0; i< result.length; i++){       
+        this.getPostDataSubs = this.postService.getIsActiveAndOrder(this.startPosition, this.limit, true).subscribe(result => {
+            for (let i = 0; i < result.length; i++) {
+                this.getCountLikeDataSubs = this.likeService.getUserLikePost(result[i].id, this.myId).subscribe(userLike => {
+                    result[i].likeId = userLike.likeId
+                    result[i].countOfLike = userLike.countOfLike
+                })
                 this.addData(result[i])
             }
         })
+
+
+        console.log(this.post)
     }
 
-    addData(post:any) {
-        
-        this.getUserDataSubs = this.userService.getById(post.createdBy).subscribe(resultUser =>{
-            
+    initLike() {
+        // this.getLikeDataSubs = this.likeService.getByUser(this.myId, this.startPosition, this.limit).subscribe(result => {
+
+        // })
+    }
+
+    addData(post: any) {
+
+        this.getUserDataSubs = this.userService.getById(post.createdBy).subscribe(resultUser => {
             this.userObj = resultUser;
             post.userName = resultUser.fullName
             post.userId = resultUser.id
@@ -123,11 +160,11 @@ export class HomeComponent implements OnInit, OnDestroy {
             this.user.push(this.userObj)
         })
 
-        this.getPostAttachmentDataSubs = this.postAttachmentService.getByPost(post.id).subscribe(result =>{
+        this.getPostAttachmentDataSubs = this.postAttachmentService.getByPost(post.id).subscribe(result => {
             post.postAttachment = result
         })
-        this.post.push(post)
 
+        this.post.push(post)
         console.log(post)
     }
 
@@ -138,7 +175,7 @@ export class HomeComponent implements OnInit, OnDestroy {
                 this.fileArray.push({ ext: result[0], files: result[1] })
             })
         }
-        console.log(this.fileArray);
+        //console.log(this.fileArray);
     }
 
     async fileUploadMultiple(event: any, index: number) {
@@ -172,10 +209,51 @@ export class HomeComponent implements OnInit, OnDestroy {
         })
         this.postForm.value.file = this.fileArray;
 
-        console.log(this.postForm.value)
+        //console.log(this.postForm.value)
         this.postInsertSubs = this.postService.insert(this.postForm.value).subscribe(() => {
             this.display = false
         })
+    }
+
+    actLike(postId: string, likeId: string) {
+        if (likeId) {
+            // this.activatedRoute.params.subscribe(result=>{
+
+            // })
+            this.getIdLikeDataSubs = this.likeService.getById(likeId).subscribe(result => {
+                if (result.isActive) {
+                    this.updateLike.controls.isActive.setValue(false)
+                } else {
+                    this.updateLike.controls.isActive.setValue(true)
+                }
+                this.updateLike.controls['id'].setValue(result.id)
+
+
+                this.updateLikeDataSubs = this.likeService.update(this.updateLike.value).subscribe(() => {
+                    for (let i = 0; i < this.post.length; i++) {
+                        this.getCountLikeDataSubs = this.likeService.getUserLikePost(this.post[i].id, this.myId).subscribe(userLike => {
+                            this.post[i].likeId = userLike.likeId
+                            this.post[i].countOfLike = userLike.countOfLike
+                        })
+                    }
+                })
+            })
+        } else {
+            this.addLike.patchValue({
+                post: {
+                    id: postId
+                }
+            })
+
+            this.insertLikeDataSubs = this.likeService.insert(this.addLike.value).subscribe(() => {
+                for (let i = 0; i < this.post.length; i++) {
+                    this.getCountLikeDataSubs = this.likeService.getUserLikePost(this.post[i].id, this.myId).subscribe(userLike => {
+                        this.post[i].likeId = userLike.likeId
+                        this.post[i].countOfLike = userLike.countOfLike
+                    })
+                }
+            })
+        }
     }
 
     ngOnDestroy(): void {
@@ -186,6 +264,11 @@ export class HomeComponent implements OnInit, OnDestroy {
 
         this.getUserDataSubs?.unsubscribe()
         this.getPostAttachmentDataSubs?.unsubscribe()
+        this.getCountLikeDataSubs?.unsubscribe()
+
+        this.insertLikeDataSubs?.unsubscribe()
+        this.updateLikeDataSubs?.unsubscribe()
+        this.getIdLikeDataSubs?.unsubscribe()
     }
 
 }
