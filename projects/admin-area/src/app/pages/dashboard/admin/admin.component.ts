9be801128a36562a2d6a9e8d3eb9b14079a5c1e0
irwansyah_10010 <irwanbae33@gmail.com>
2022-12-10 19:04:59 +0700

    update count

--- projects/admin-area/src/app/pages/dashboard/admin/admin.component.ts
+++ projects/admin-area/src/app/pages/dashboard/admin/admin.component.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy, OnInit } from "@angular/core"
 import { ArticleService } from "projects/main-area/src/app/service/article.service"
 import { ApiService } from "projects/main-area/src/app/service/api.service"
 import { Subscription } from "rxjs"
+import { UserService } from "projects/main-area/src/app/service/user.service"
+import { PaymentActivityDetailService } from "projects/main-area/src/app/service/payment-activity-detail.service"
 
 @Component({
     selector: 'admin-dashboard',
@@ -10,11 +12,21 @@ import { Subscription } from "rxjs"
 export class AdminComponent implements OnDestroy, OnInit {
     fullName!: string
     totalArticle!: number
+    totalUserSubsribe!:number
+    totalApproveEvent!:number
+    totalApproveCourse!:number
+
+    private totalApprovedAndActivityTypeEvent?:Subscription
+    private totalApprovedAndActivityTypeCourse?:Subscription
 
     private articleGetCountSubscription?: Subscription
+    private userSubscribeGetCountSubscription?: Subscription
+    
 
     constructor(private articleService: ArticleService,
-        private apiService: ApiService) { }
+        private userService:UserService,
+        private apiService: ApiService,
+        private paymentActivityDetailService: PaymentActivityDetailService) { }
 
     ngOnInit(): void {
         this.fullName = String(this.apiService.getName())
@@ -22,10 +34,25 @@ export class AdminComponent implements OnDestroy, OnInit {
         this.articleGetCountSubscription = this.articleService.getTotalArticle().subscribe(result => {
             this.totalArticle = result.countOfArticle
         })
+
+        this.userSubscribeGetCountSubscription = this.userService.getTotalSubscribe().subscribe(result=>{
+            this.totalUserSubsribe = result.countOfUser
+        })
+
+        this.totalApprovedAndActivityTypeEvent = this.paymentActivityDetailService.getTotalByPaymentApprovedAndActivityTypeId('event').subscribe(result=>{
+            this.totalApproveEvent = result.countOfPaymentActivity
+        })
+
+        this.totalApprovedAndActivityTypeCourse = this.paymentActivityDetailService.getTotalByPaymentApprovedAndActivityTypeId('course').subscribe(result=>{
+            this.totalApproveCourse = result.countOfPaymentActivity
+        })
     }
 
     ngOnDestroy(): void {
         this.articleGetCountSubscription?.unsubscribe()
+        this.userSubscribeGetCountSubscription?.unsubscribe()
+        this.totalApprovedAndActivityTypeCourse?.unsubscribe()
+        this.totalApprovedAndActivityTypeEvent?.unsubscribe()
     }
 
 }
